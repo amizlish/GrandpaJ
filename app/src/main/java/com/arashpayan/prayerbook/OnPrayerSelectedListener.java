d99601c124725fb81e1d5a0a44333ac4f31c71b7
amitm <amit.mizlish@kenshoo.com>
2017-12-26 18:10:11 +0200

    main menu add

--- app/src/main/java/com/arashpayan/prayerbook/OnPrayerSelectedListener.java
+++
@@ -1,9 +0,0 @@
-package com.arashpayan.prayerbook;
-
-/**
- * PrayerBook
- * Created by arash on 5/2/16.
- */
-public interface OnPrayerSelectedListener {
-    void onPrayerSelected(long prayerId);
-}
